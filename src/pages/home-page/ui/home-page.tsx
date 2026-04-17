5109a72fa58ec282bd4f7d893623ed1ac11cb576
Diogo Cunha <diogocunha.jlle@gmail.com>
2026-04-17 10:14:00 -0300

    feat: Desenvolver Toaster (#4)

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,5 +1,22 @@
+import { toaster } from '~/shared';
+
 const HomePage = () => {
-	return <div>Home Page</div>;
+	return (
+		<div>
+			Home Page
+			<button
+				onClick={() => {
+					toaster({
+						title: 'test',
+						message: 'test',
+						type: 'success',
+					});
+				}}
+			>
+				CLICK
+			</button>
+		</div>
+	);
 };
 
 export default HomePage;
---
+++ src/shared/lib/alerts/index.ts
@@ -0,0 +1,1 @@
+export { default as toaster } from './ui/toaster';
