c6de81944f5ffd492e0d34bcfd2ec32eec44af82
DiogooCunha <diogocunhajlle@gmail.com>
2026-04-29 14:01:56 -0300

    feat: add HTMLHead component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App.tsx';
+import { HTMLHead } from '~/shared';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root not found');
+
+const root = createRoot(rootElement);
+
+root.render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<HTMLHead />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,3 +4,4 @@ export { default as Checkbox } from './checkbox';
 export { default as Typography } from './typography';
 export { default as Button } from './button';
 export { default as Box } from './box';
+export { default as HTMLHead } from './html-head';
