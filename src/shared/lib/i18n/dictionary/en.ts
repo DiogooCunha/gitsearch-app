72161c41beaae729e5baa707216c25bfcf30379e
João Vitor da Silva Vieira <joao.svieira@selbetti.local>
2026-04-30 19:43:56 -0300

    feat: add translate

--- src/shared/lib/i18n/dictionary/en.ts
+++ src/shared/lib/i18n/dictionary/en.ts
@@ -2,5 +2,10 @@ export const enTranslations = {
 	notFoundPage: {
 		pageNotFound: 'Page not found',
 		pageDoesntExist: "The page you were looking for doesn't exist",
+		backToHome: 'Back to Home'
 	},
+	navbar: {
+		notification: 'Notifications',
+		github: 'Redirect to GitHub'
+	}
 };
--- src/shared/lib/i18n/dictionary/pt.ts
+++ src/shared/lib/i18n/dictionary/pt.ts
@@ -2,6 +2,10 @@ export const ptTranslations = {
 	notFoundPage: {
 		pageNotFound: 'Página não encontrada',
 		pageDoesntExist: 'A página que você busca não existe',
-		backToHome: 'Voltar para Login',
+		backToHome: 'Voltar para o Login'
 	},
+	navbar: {
+		notification: 'Notificações',
+		github: 'Redirecionar para GitHub'
+	}
 };
